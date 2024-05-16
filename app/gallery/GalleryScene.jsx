016a8836db61ef4e5c5a1aaead70dadb1432dc54
Dirty Martini <hello@maxwellkirwin.co.uk>
2024-05-16 21:52:56 +0100

    snapping nearly there

--- app/gallery/GalleryScene.jsx
+++ app/gallery/GalleryScene.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { act, useEffect, useRef, useState } from "react"
 import { useFrame, useThree } from "@react-three/fiber"
 import { useScroll, Image } from "@react-three/drei"
 import { easing } from "maath"
@@ -13,63 +13,117 @@ export default function GalleryScene({ children, ...props }) {
   const { viewport } = useThree()
 
   const { galleryProps } = props
+  let activeIndex = 0
 
   const sliderMargin = galleryProps.margin
-  const sliderLength = 6
+
   let sliderWidth = viewport.width / 3
 
   // 8 images
   const images = [1, 2, 3, 4, 5, 1, 2, 3]
+  const sliderLength = images.length
+  const imagePositions = []
 
   // make sure we have the smae amount of imageRefs as images
   useEffect(() => {
     imagesRef.current = imagesRef.current.slice(0, images.length)
+
+    // set initial position
+    imagesRef.current.forEach((image, index) => {
+      imagePositions[index] = index * (sliderWidth + sliderMargin)
+    })
   })
+
+  console.log("imagePositions", imagePositions)
 
   useFrame((state, delta) => {
     // move plane according to camera scroll
 
     // loop through images and update their position
     imagesRef.current.forEach((image, index) => {
-      let initialPosition = index * (sliderWidth + sliderMargin)
-      // goes 0 - 1 - 0 (1) in the middle
-      const curve = scroll.curve(0, 1)
-
-      // 1 needs to be furthest away
-      // initial position index * (sliderWidth + sliderMargin)
-
       easing.damp(
         image.position,
         "x",
-        initialPosition -
-          //  (sliderWidth + sliderMargin) - // offset to make first slide be in middle
+        imagePositions[index] -
+          // (sliderWidth + sliderMargin) - // offset to make first slide be in middle
           scroll.offset *
-            (sliderLength + 1) * // when offset is in middle * (sliderLength - 1)
+            (sliderLength - 1) * // when offset is in middle * (sliderLength - 1)
             (sliderWidth + sliderMargin),
         0.15,
         delta
       )
 
       // if image position is 1 or less or -1 or more than update greyscale value to 1
-      if (Math.abs(image.position.x) > 2) {
+      if (Math.abs(image.position.x) > 1) {
         //   image.material.color.set("grey")
         easing.damp(image.material, "grayscale", 1, 0.45, delta)
-        easing.damp(image.material, "opacity", 0.25, 0.45, delta)
+        easing.damp(image.material, "opacity", 0.15, 0.45, delta)
       } else {
+        // this is active!
+
         easing.damp(image.material, "grayscale", 0, 0.45, delta)
         easing.damp(image.material, "opacity", 1, 0.45, delta)
+
+        activeIndex = index
+        console.log((sliderMargin + sliderWidth) * (index - activeIndex))
       }
 
       // snap scroll on scroll
-      console
+
+      // snap to active
+
+      if (scroll.delta === 0) {
+        // console.log("activeIndex", activeIndex)
+
+        //* if active index 6
+        //  sliderLength = 8 - 1
+
+        // 0 = sliderMargin + sliderWidth * -7
+        // 1 = sliderMargin + sliderWidth * -6
+        // 2 = sliderMargin + sliderWidth * -5
+        // 3 = sliderMargin + sliderWidth * -3
+        // 4 = sliderMargin + sliderWidth * -2
+        // 5 = sliderMargin + sliderWidth * -1
+        // 6 = sliderMargin + sliderWidth * 0
+        // 7 = sliderMargin + sliderWidth * 1
+
+        // if activeIndex is 6 how do we get to 0 using sliderLength and sliderWidth + sliderMargin
+
+        // if activeIndex is 0
+        // 0 = sliderMargin + sliderWidth * index (0) - activeIndex (0)
+        // 1 = sliderMargin + sliderWidth * index (1) - activeIndex (0)
+        // 2 = sliderMargin + sliderWidth * index (2) - activeIndex (0)
+        // 3 = sliderMargin + sliderWidth * index (3) - activeIndex (0)
+        // 4 = sliderMargin + sliderWidth * index (4) - activeIndex (0)
+        // 5 = sliderMargin + sliderWidth * index (5) - activeIndex (0)
+        // 6 = sliderMargin + sliderWidth * index (6) - activeIndex (0)
+        // 7 = sliderMargin + sliderWidth * index (7) - activeIndex (0)
+
+        // if activeIndex is 1
+        // index (0) - activeIndex (1) = -1
+        // index (1) - activeIndex (1) = 0
+        // index (2) - activeIndex (1) = 1
+
+        // if activeIndex is 2
+        // index (0) - activeIndex (2) = -2
+        // index (1) - activeIndex (2) = -1
+        // index (2) - activeIndex (2) = 0
+        // index (3) - activeIndex (2) = 1
+
+        image.position.x = (sliderMargin + sliderWidth) * (index - activeIndex)
+
+        // MAKE THIS WORK
+        // easing.damp(
+        //   image.position,
+        //   "x",
+        //   (sliderMargin + sliderWidth) * (index - activeIndex),
+        //   0.15,
+        //   delta
+        // )
+      }
+
       // snap scroll on click
     })
-  })
-
-  const material = new THREE.MeshBasicMaterial({
-    color: "white",
-    wireframe: true,
-    side: THREE.DoubleSide,
   })
 
   return (
